325f99bfad5b83a7ea95f6ea8958ece595d9abe7
hungngocphat01l <hungngocphat01@gmail.com>
2024-04-24 14:51:59 +0700

    [logger] fix: log moving average instead of step-wise metrics

--- nano_seq/logger.py
+++ nano_seq/logger.py
@@ -3,6 +3,18 @@ from collections import defaultdict
 
 
 class Logger(ABC):
+    def __init__(self):
+        self.step = 0
+        self._epoch = 1
+
+    @property
+    def epoch(self):
+        return self._epoch
+
+    @epoch.setter
+    def epoch(self, value: int):
+        pass
+
     @abstractmethod
     def write_train(self, batch_idx: int, **kwargs):
         pass
@@ -14,24 +26,49 @@ class Logger(ABC):
 
 class SimpleLogger(Logger):
     def __init__(self):
-        self.step = 0
-        self.epoch = 0
-        self.train_metrics = []
-        self.eval_metrics = []
+        super().__init__()
+        self.train_metrics = {}
+        self.eval_metrics = {}
+
+    @property
+    def epoch(self):
+        return self._epoch
+
+    @epoch.setter
+    def epoch(self, value: int):
+        self._epoch = value
+
+        first_value = {"step": -1, "value": 0}
+        self.train_metrics[value] = defaultdict(lambda: [first_value])
+        self.eval_metrics[value] = defaultdict(lambda: [first_value])
 
     def write_train(self, batch_idx: int, **kwargs):
-        self.train_metrics.append({"epoch": self.epoch, "batch_idx": batch_idx, "step": self.step, **kwargs})
+        moving_avg = {}
+
+        for name, value in kwargs.items():
+            metric_arr = self.train_metrics[self.epoch][name]
+            avg_value = (metric_arr[-1]["value"] * batch_idx + value) / (batch_idx + 1)
+            moving_avg[name] = avg_value
+            metric_arr.append(
+                {
+                    "step": self.step,
+                    "value": avg_value,
+                }
+            )
         self.step += 1
 
         log_str = "Epoch {} | Step {} ".format(self.epoch, self.step)
-        for name, value in kwargs.items():
+        for name, value in moving_avg.items():
             log_str += "| {}: {} ".format(name, value)
+
         print(log_str)
 
     def write_eval(self, **kwargs):
-        self.eval_metrics.append({"epoch": self.epoch, "step": self.step, **kwargs})
+        for name, value in kwargs.items():
+            self.eval_metrics[self.epoch][name].append({"step": self.step, "value": value})
 
-        log_str = "Epoch {} | Step {} ".format(self.epoch, self.step)
+        log_str = "Validation | Epoch {} | Step {} ".format(self.epoch, self.step)
         for name, value in kwargs.items():
             log_str += "| {}: {} ".format(name, value)
+
         print(log_str)
