6d36c638a50e9b2196bc18cba96793a2118b8645
hungngocphat01l <hungngocphat01@gmail.com>
2024-05-06 14:28:51 +0700

    refactor: change individual function to pytest.parameterize

--- tests/data/test_utils.py
+++ tests/data/test_utils.py
@@ -28,14 +28,10 @@ def expected_decoder_mask_left():
     )
 
 
-def test_pad_sequence_left():
-    out = pad_sequence([1, 2, 3], pad_idx=5, max_len=6, pad="left")
-    assert out == [5, 5, 5, 1, 2, 3]
-
-
-def test_pad_sequence_right():
-    out = pad_sequence([1, 2, 3], pad_idx=5, max_len=6, pad="right")
-    assert out == [1, 2, 3, 5, 5, 5]
+@pytest.mark.parametrize("pad,expected", [("left", [5, 5, 5, 1, 2, 3]), ("right", [1, 2, 3, 5, 5, 5])])
+def test_pad_sequence(pad: str, expected: list):
+    out = pad_sequence([1, 2, 3], pad_idx=5, max_len=6, pad=pad)
+    assert out == expected
 
 
 def test_get_padding_mask(padded_input):
@@ -50,30 +46,30 @@ def test_get_padding_mask(padded_input):
     assert torch.equal(mask, expected_mask)
 
 
-def test_get_future_mask_left(padded_input):
-    mask = get_future_mask(padded_input, padding="left")
+@pytest.mark.parametrize("padding,expected_mask", [
+    ("left", torch.flip(torch.tril(torch.ones(6, 6)), dims=[1])),
+    ("right", torch.tril(torch.ones(6, 6))),
+])
+def test_get_future_mask_left(padding, padded_input, expected_mask):
+    if padding == "right":
+        padded_input = torch.flip(padded_input, dims=[1])
+
+    mask = get_future_mask(padded_input, padding=padding)
 
     assert mask.ndim == 4
     assert mask.size(2) == mask.size(3) == len(padded_input[0])
 
-    expected_mask = torch.flip(torch.tril(torch.ones((6, 6))), dims=[1]).unsqueeze(0).unsqueeze(0).long()
-
+    expected_mask = expected_mask.unsqueeze(0).unsqueeze(0).long()
     assert torch.equal(expected_mask, mask)
 
 
-def test_get_decoder_mask_left(padded_input, expected_decoder_mask_left):
-    mask = get_decoder_mask(padded_input, pad_idx=7)
-    assert torch.equal(mask[0], expected_decoder_mask_left[0])
+@pytest.mark.parametrize("padding", ["left", "right"])
+def test_get_decoder_mask(padding, padded_input, expected_decoder_mask_left):
+    expected_mask = expected_decoder_mask_left[0]
 
+    if padding == "right":
+        padded_input = torch.flip(padded_input, dims=[1])
+        expected_mask = torch.flip(expected_mask, dims=[2])
 
-def test_get_future_mask_right(padded_input):
-    # Does not need to flip input, since future mask only need the dimensions, not the data
-    mask = get_future_mask(padded_input, padding="right")
-    expected_mask = torch.tril(torch.ones((6, 6))).unsqueeze(0).unsqueeze(0).long()
-    assert torch.equal(expected_mask, mask)
-
-
-def test_get_decoder_mask_right(padded_input, expected_decoder_mask_left):
-    flip_input = torch.flip(padded_input, dims=[1])
-    mask = get_decoder_mask(flip_input, pad_idx=7, padding="right")
-    assert torch.equal(mask[0], torch.flip(expected_decoder_mask_left[0], dims=[2]))
+    mask = get_decoder_mask(padded_input, pad_idx=7, padding=padding)
+    assert torch.equal(mask[0], expected_mask)
