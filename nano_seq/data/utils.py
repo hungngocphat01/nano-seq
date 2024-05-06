5df05a5408798c232518305cdf6ece34780797a0
hungngocphat01l <hungngocphat01@gmail.com>
2024-05-06 13:54:47 +0700

    feat: add new get_decoder_mask, rename get_encoder_mask

--- nano_seq/data/utils.py
+++ nano_seq/data/utils.py
@@ -1,5 +1,7 @@
+from numpy import pad
 import torch
 from .const import PAD
+
 
 def pad_sequence(seq: list[int], pad_idx: int, max_len: int, pad="left"):
     """
@@ -30,7 +32,7 @@ def pad_sequence(seq: list[int], pad_idx: int, max_len: int, pad="left"):
         return seq + pad_segment
 
 
-def get_encoder_mask(x: torch.Tensor, pad_idx=PAD):
+def get_padding_mask(x: torch.Tensor, pad_idx=PAD):
     """
     Args
     ----
@@ -43,3 +45,44 @@ def get_encoder_mask(x: torch.Tensor, pad_idx=PAD):
     """
     assert x.ndim == 2, "input must have size [bsz, max_len]. got only {} dimensions".format(x.ndim)
     return (x != pad_idx).unsqueeze(1).unsqueeze(1).long()
+
+
+def get_future_mask(x: torch.Tensor, padding = "left"):
+    """
+    Args
+    ----
+    x: tensor
+        shape [bsz, max_len]
+
+    Return
+    ------
+    tensor of shape [1, 1, max_len, max_len]
+        with value 1 where the token is not pad
+    """
+    assert x.ndim == 2, "input tensor must have size [bsz, max_len]. got only {} dimensions".format(x.ndim)
+    max_len = x.size(1)
+
+    mask = torch.tril(torch.ones((max_len, max_len)))
+    if padding == "left":
+        mask = torch.flip(mask, dims=[1])
+
+    return mask.unsqueeze(0).unsqueeze(0).long()
+
+
+def get_decoder_mask(x: torch.Tensor, padding = "left", pad_idx=PAD):
+    """
+    Args
+    ----
+    x: tensor
+        shape [bsz, max_len]
+
+    Return
+    ------
+    tensor of shape [bsz, 1, max_len, max_len]
+        with value 1 where the token is not pad
+    """
+    padding_mask = get_padding_mask(x, pad_idx)
+    future_mask = get_future_mask(x, padding)
+
+    # 0 (padded) takes precedence
+    return torch.minimum(padding_mask, future_mask)
