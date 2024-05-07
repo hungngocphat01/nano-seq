565e2770eac3499c323d4349e6cc9e911b53b729
hungngocphat01l <hungngocphat01@gmail.com>
2024-05-07 17:13:20 +0700

    [trainer] refactor: split batch iteration logic to trainer class

--- nano_seq/trainer.py
+++ nano_seq/trainer.py
@@ -1,25 +1,53 @@
+from typing import Iterable
 import torch
 from torch import nn
 
 from nano_seq.logger import Logger
+from nano_seq.task.base import BaseTask
 
 
-def train(
-    epochs: int,
-    model: nn.Module,
-    train_iter,
-    valid_iter,
-    optimizer: torch.optim.Optimizer,
-    criterion,
-    train_func,
-    valid_func,
-    logger: Logger
-):
-    for i in range(epochs):
-        logger.epoch = i + 1
+class Trainer:
+    def __init__(
+        self,
+        train_iter: Iterable,
+        eval_iter: Iterable,
+        optimizer: torch.optim.Optimizer,
+        criterion: nn.Module,
+        task: BaseTask,
+        model: torch.nn.Module,
+        logger: Logger,
+    ):
+        self.train_iter = train_iter
+        self.eval_iter = eval_iter
+        self.optimizer = optimizer
+        self.criterion = criterion
+        self.task = task
+        self.model = model
+        self.logger = logger
 
-        model.train()
-        train_func(train_iter, model, optimizer, criterion, logger)
+    def train_epoch(self):
+        for batch_idx, sample in enumerate(iter(self.train_iter)):
+            _, label = sample
+            net_input = self.task.get_net_input(sample)
 
-        model.eval()
-        valid_func(valid_iter, model, criterion, logger)
+            logs = self.task.train_step(net_input, label, self.model, self.optimizer, self.criterion)
+            self.optimizer.step()
+
+            self.logger.write_train(batch_idx, **logs)
+
+    def eval_epoch(self):
+        for batch_idx, sample in enumerate(iter(self.eval_iter)):
+            _, label = sample
+            net_input = self.task.get_net_input(sample)
+
+            with torch.no_grad():
+                logs = self.task.eval_step(net_input, label, self.model, self.criterion)
+
+            self.logger.write_eval(batch_idx, **logs)
+
+    def start_train(self, num_epochs: int):
+        for i in range(num_epochs):
+            self.logger.epoch = i + 1
+
+            self.train_epoch()
+            self.eval_epoch()
